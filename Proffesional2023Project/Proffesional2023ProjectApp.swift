01462274390833f7617397b3c0ab24d4807ea13d
nke <nke@nke.ru>
2023-06-27 13:49:24 +0700

    Initial Commit

---
+++ Proffesional2023Project/Proffesional2023ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proffesional2023ProjectApp.swift
+//  Proffesional2023Project
+//
+//  Created by admin on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proffesional2023ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
